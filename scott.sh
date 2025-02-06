89b8f2fe4d6feab58bee507417a4ba5861c85a21
enjeeneer <scottjeen@gmail.com>
2025-02-06 14:38:50 +0000

    smaller batch size

--- scott.sh
+++ scott.sh
@@ -1,13 +1,13 @@
 PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
  data.train_files=../drive/MyDrive/data/gsm8k/train.parquet \
  data.val_files=../drive/MyDrive/data/gsm8k/test.parquet \
- data.train_batch_size=256 \
+ data.train_batch_size=64 \
  data.val_batch_size=1312 \
  data.max_prompt_length=512 \
  data.max_response_length=256 \
  actor_rollout_ref.model.path=../drive/MyDrive/models/Qwen2.5-0.5B-Instruct \
  actor_rollout_ref.actor.optim.lr=1e-6 \
- actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+ actor_rollout_ref.actor.ppo_mini_batch_size=16 \
  +actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
  +actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=8 \
  actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
