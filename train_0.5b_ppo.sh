7f287e5bf4d36a1342777d56d263bee7f7660c8a
enjeeneer <scottjeen@gmail.com>
2025-02-06 11:56:28 +0000

    updated train script

--- train_0.5b_ppo.sh
+++ train_0.5b_ppo.sh
@@ -6,10 +6,10 @@
 export N_GPUS=1
 export CUDA_VISIBLE_DEVICES=2
 ray stop --force && ray start --head --include-dashboard=False
-export BASE_MODEL="/content/TinyZero/model/Qwen2.5-0.5"
-export DATA_DIR="/content/TinyZero/data/countdown"
+export BASE_MODEL="../drive/MyDrive/models/Qwen2.5-0.5B-Instruct"
+export DATA_DIR="../drive/MyDrive/data/countdown"
 export ROLLOUT_TP_SIZE=1
-export EXPERIMENT_NAME=countdown-qwen2.5-0.5b-countdown
+export EXPERIMENT_NAME=countdown-qwen2.5-0.5b-instruct-countdown
 export VLLM_ATTENTION_BACKEND=XFORMERS
 
 bash ./scripts/train_tiny_zero_a100_ppo.sh
