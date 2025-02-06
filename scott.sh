05b3724e0e4fab38ea7e1824b525d18e957e3a01
enjeeneer <scottjeen@gmail.com>
2025-02-06 15:13:16 +0000

    micro batch size

--- scott.sh
+++ scott.sh
@@ -8,6 +8,7 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo \
  actor_rollout_ref.model.path=../drive/MyDrive/models/Qwen2.5-0.5B-Instruct \
  actor_rollout_ref.actor.optim.lr=1e-6 \
  actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+ actor_rollout_ref.actor.ppo_micro_batch_size=32 \
  +actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
  +actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=1 \
  actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
